04cec1e681c8fa07b9c4accaa8002ec6e938333f
belaventer <isaventer1@hotmail.com>
2021-05-26 10:48:34 +0000

    Add Register functionality

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
-from flask import (Flask, render_template)
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,8 +25,34 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("new_username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("new_username").lower(),
+            "password": generate_password_hash(
+                request.form.get("new_password")),
+            "admin": False
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("new_username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("register"))
     return render_template("login.html")
 
 
